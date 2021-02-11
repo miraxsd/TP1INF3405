9e666697d3018d54b23fd5d2e519aeac4747c370
miraxsd <60142429+miraxsd@users.noreply.github.com>
2021-02-10 20:53:00 -0500

    Entrée de commandes
    Co-Authored-By: sasafc <60142097+sasafc@users.noreply.github.com>
    Co-Authored-By: maddietjiu <77162816+maddietjiu@users.noreply.github.com>

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -9,112 +9,112 @@ import java.util.Scanner;
 public class Client {
 
 	private static Socket socket;
-		
-	/* Application client
+
+	/*
+	 * Application client
 	 */
-	public static void main (String[] args) throws Exception
-	{
+	public static void main(String[] args) throws Exception {
 		// Address et port du serveur
 		System.out.println("Enter IP address :");
 		Scanner sc = new Scanner(System.in);
 		String serverAddress = sc.next();
-		
-		while (!ipvalide(serverAddress))
-		{
+
+		while (!ipvalide(serverAddress)) {
 			System.out.println("IP adress invalid. Please enter another IP adress :");
-			serverAddress=sc.next();
+			serverAddress = sc.next();
 		}
 		System.out.println("Enter port number :");
-		int port=0;
-		while((port<5000) || (port>5050)) {
+		int port = 0;
+		while ((port < 5000) || (port > 5050)) {
 			try {
 				port = sc.nextInt();
-			}catch(java.util.InputMismatchException e) {
+			} catch (java.util.InputMismatchException e) {
 				System.out.println("Invalid port. Please enter another port between 5000 and 5050 :");
 			}
-			if(port<5000 || port>5050)
+			if (port < 5000 || port > 5050)
 				System.out.println("Invalid port. Please enter another port between 5000 and 5050 :");
 		}
-		
-		
-		if(socket!=null)
+
+		if (socket != null)
 			socket.close();
 		// 1. Création d'une nouvelle connexion avec le serveur
 		socket = new Socket(serverAddress, port);
-		System.out.println("The server is running on " + serverAddress+ ":"+ port);
-		
+		System.out.println("The server is running on " + serverAddress + ":" + port);
+
 		// Création d'un canal entrant pour recevoir les messages envoyés par le serveur
 		DataInputStream in = new DataInputStream(socket.getInputStream());
-					
+
 		// Attente de la réception d'un message envoyé par le serveur sur le canal
 		String helloMessageFromServer = in.readUTF();
 		System.out.println(helloMessageFromServer);
-		
-		// Création d'un canal sortant pour écrire au serveur 
-		DataOutputStream dos = new DataOutputStream(socket.getOutputStream()); 
-		
-		// Read data coming from the server 
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream())); 
 
-		// Read data from the keyboard 
-		BufferedReader kb = new BufferedReader( new InputStreamReader(System.in)); 
-		String strKb, strSv; 
+		// Création d'un canal sortant pour écrire au serveur
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
-				// repeat as long as exit 
-				// is not typed at client 
-				while (!(strKb = kb.readLine()).equals("exit")) { 
-
-					// send to the server 
-					dos.writeBytes(strKb + "\n"); 
-					
-					// receive from the server 
-					strSv = br.readLine(); 
-					//strSv = in.readUTF(); 
-					System.out.println(strSv); 
-					}
+		// Read data coming from the server
+		// BufferedReader br = new BufferedReader(new
+		// InputStreamReader(socket.getInputStream()));
 		
-				
+		String strKb;
+
+		// repeat as long as exit
+		// is not typed at client
+		while (!(strKb = sc.next()).equals("exit")) {
+
+			// send to the server
+			out.writeUTF(strKb);
+
+			// receive from the server
+			readMessagesFromServer(in);
+		}
+
 		// Fermeture de la connexion avec le serveur
-				socket.close();
-				// close connections with read/write 
-				dos.close(); 
-				br.close(); 
-				kb.close(); 
-		//}catch(java.net.BindException e) { socket.close();}
+		socket.close();
+		// close connections with read/write
+		out.close();
+		// br.close();
+		// }catch(java.net.BindException e) { socket.close();}
 		sc.close();
 	}
-	public static boolean ipvalide (String serverAddress) {
-		if(serverAddress.endsWith(".")) 
+
+	public static boolean ipvalide(String serverAddress) {
+		if (serverAddress.endsWith("."))
 			return false;
-	
-		String [] checkSA = serverAddress.split("\\.");
-		
-		int ipLength = checkSA.length; 
-		int compteur =0;
-		
-		if(ipLength !=4)
+
+		String[] checkSA = serverAddress.split("\\.");
+
+		int ipLength = checkSA.length;
+		int compteur = 0;
+
+		if (ipLength != 4)
 			return false;
-		while(compteur < 4) {
+		while (compteur < 4) {
 			try {
-				if (Integer.parseInt(checkSA[compteur])<=255 && Integer.parseInt(checkSA[compteur])>=0 ) 
+				if (Integer.parseInt(checkSA[compteur]) <= 255 && Integer.parseInt(checkSA[compteur]) >= 0)
 					compteur++;
-				else 
+				else
 					return false;
-			}catch(NumberFormatException e) {
+			} catch (NumberFormatException e) {
 				return false;
 			}
-				
+
 		}
-	
+
 		return true;
 	}
-	
-	
-	
+
+	public static void readMessagesFromServer(DataInputStream in) throws java.io.IOException {
+		try {
+			while (true) {
+				String helloMessageFromServer = "";
+				if ((helloMessageFromServer = in.readUTF()).equals("end"))
+					break;
+				System.out.println(helloMessageFromServer);
+			}
+		} catch (java.io.EOFException ignore) {
+		}
+		;
+
+	}
+
 }
-
-
-
-
-
-
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -11,7 +11,7 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
-import java.io.InputStreamReader; 
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -23,138 +23,153 @@ import java.util.stream.Stream;
 public class Server {
 
 	private static ServerSocket listener;
-	
+
 	/* Application Serveur */
-	
-	public static void main(String[] args) throws Exception
-	{
-	
+
+	public static void main(String[] args) throws Exception {
+
 		// Compteur incrémenté à chaque connexion d'un client au serveur
 		int clientNumber = 0;
-				
+
 		// Adresse et port du serveur
 		String serverAddress = "127.0.0.1";
 		int serverPort = 5000;
-		
+
 		// Création de la connexion pour communiquer avec les clients
 		listener = new ServerSocket();
 		listener.setReuseAddress(true);
 		InetAddress serverIP = InetAddress.getByName(serverAddress);
-		
+
 		// Association de l'adresse et du port à la connexion
 		listener.bind(new InetSocketAddress(serverIP, serverPort));
 		System.out.format("The server is running on %s:%d%n", serverAddress, serverPort);
-		
-		try
-		{
-			/* À chaque fois qu'un nouveau client se connecte, on exécute la fonction Run() de l'objet
-			 * ClientHandler.
+
+		try {
+			/*
+			 * À chaque fois qu'un nouveau client se connecte, on exécute la fonction Run()
+			 * de l'objet ClientHandler.
 			 */
-			while(true) 
-			{
-				// Important: la fct accept() est bloquante : attend qu'un prochain client se connecte
+			while (true) {
+				// Important: la fct accept() est bloquante : attend qu'un prochain client se
+				// connecte
 				// Une nouvelle connexion : on incrémente le compteur clientNumber
 				new ClientHandler(listener.accept(), clientNumber++, serverAddress, serverPort).start();
-				
+
 			}
-			
-		}
-		finally
-		{
+
+		} finally {
 			// Fermeture de la connexion
 			listener.close();
 		}
 	}
 
-	/* Une thread qui se charge de traiter la demande de chaque client
-	 * sur un socket particulier.
+	/*
+	 * Une thread qui se charge de traiter la demande de chaque client sur un socket
+	 * particulier.
 	 */
-	private static class ClientHandler extends Thread
-	{
+	private static class ClientHandler extends Thread {
 		private Socket socket;
 		private int clientNumber;
 		private String serverAddress;
 		private int serverPort;
-	
-  
-		public ClientHandler(Socket socket, int clientNumber, String serverAddress, int serverPort) throws IOException
-		{
+
+		public ClientHandler(Socket socket, int clientNumber, String serverAddress, int serverPort) throws IOException {
 			this.socket = socket;
 			this.clientNumber = clientNumber;
 			this.serverAddress = serverAddress;
-			System.out.println("New connection with client#"+ clientNumber + " at "+ socket);
-			       
+			System.out.println("New connection with client#" + clientNumber + " at " + socket);
+
 		}
-		/* Une thread qui se charge d'envoyer au client un message de bienvenue*/
-		public void run()
-		{
-			
-			try
-			{
-		        // to send data to the client 
-		        PrintStream ps = new PrintStream(socket.getOutputStream()); 
-		  
-		        // to read data coming from the client 
-		        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream())); 
-		        //DataInputStream in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-		        
-		        // to read data from the keyboard 
-		        BufferedReader kb = new BufferedReader(new InputStreamReader(System.in)); 
+
+		/* Une thread qui se charge d'envoyer au client un message de bienvenue */
+		public void run() {
+
+			try {
+				// to send data to the client
+				PrintStream ps = new PrintStream(socket.getOutputStream());
+				// Création d'un canal sortant pour envoyer des messages au client
+				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+				// to read data coming from the client
+				 BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				DataInputStream in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+				FileManager dir = new FileManager(System.getProperty("user.dir"));
+				// to read data from the keyboard
+				//BufferedReader kb = new BufferedReader(new InputStreamReader(Socket.in));
+				// Envoie d'un message au client
+				out.writeUTF("Hello from server - you are client #" + clientNumber + ". What would you like to do?");
 				while (true) {
+
+					String strClient="";
 					
-					// Création d'un canal sortant pour envoyer des messages au client
-					DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-					
-					// Envoie d'un message au client
-					out.writeUTF("Hello from server - you are client #" + clientNumber+". What would you like to do?");
-					
-					String strCl, strKb;
 					LocalDateTime dateTime = LocalDateTime.now();
 					DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy-MM-dd@HH:mm:ss");
 					String formattedDateTime = dateTime.format(myFormatObj);
 					int port = socket.getPort();
-					while ((strCl = br.readLine()) != null) { 
-						
-		                System.out.println("[" + serverAddress +":"+ port +" - "+ formattedDateTime + "] : "+ strCl); 
-		                
-		                
-		                ps.println("Command executed");
-		                
-		               
-		            }							
+
+					while ((strClient = in.readUTF()) != null) {
+						System.out.println(
+								"[" + serverAddress + ":" + port + " - " + formattedDateTime + "] : " + strClient);
+						// Switch Case pour les commandes
+						String command = strClient.split(" ")[0];
+						switch (command) {
+						case "ls":
+							dir.ls(out);
+							break;
+						case "cd":
+							in.readUTF();
+							out.writeUTF("Je suis cd qu'est ce que vous voulez de moi?");
+							break;
+						case "mkdir":
+							in.readUTF();
+							// create an abstract pathname de type File object
+							String path = ""; // il faut modifier cette ligne pour prendre le nom du fichier choisi par
+												// l'utilisitateur
+							File file = new File(path);
+							if (file.mkdir()) {
+								System.out.println("Directory is created");
+							} else {
+								System.out.println("Directory can't be created");
+							}
+							break;
+						case "upload":
+							in.readUTF();
+							out.writeUTF("Je suis upload qu'est ce que vous voulez de moi?");
+							break;
+						case "download":
+							in.readUTF();
+							out.writeUTF("Je suis download qu'est ce que vous voulez de moi?");
+							break;
+						case "exit":
+							out.writeUTF("Vous avez été déconnecté avec succès");
+							break;
+						default:
+							out.writeUTF("La commande n'a pas été reconnue");
+							// break;
+						}
+						// ps.println("Command executed");
+
+						out.writeUTF("end");
+						command = "";
+						// in.
+						break;
+					}
 				}
-				
-			}
-			catch (IOException  e)
-			{
-				System.out.println("Error handling client# " + clientNumber + ": "+ e);
-			}
-			finally
-			{
-				try
-				{
+
+			} catch (IOException e) {
+				System.out.println("Error handling client# " + clientNumber + ": " + e);
+			} finally {
+				try {
 					// Fermeture de la connexion avec le client
 					socket.close();
-					
+
 					// terminate
-					System.exit(0);
-		
-				}
-				catch (IOException  e)
-				{
+					// System.exit(0);
+
+				} catch (IOException e) {
 					System.out.println("Couldn't close a socket, what's going on?");
 				}
 				System.out.println("Connection with client# " + clientNumber + " closed");
 			}
 		}
-	}
-	public static Set<String> listFilesUsingFileWalk(String dir, int depth) throws IOException {
-	    try (Stream<Path> stream = Files.walk(Paths.get(dir), depth)) {
-	        return stream
-	          .filter(file -> !Files.isDirectory(file))
-	          .map(Path::getFileName)
-	          .map(Path::toString)
-	          .collect(Collectors.toSet());
-	    }
 	}
 }
