e26e7f658953726a42f8544a058a2fa8c8d8e683
maddietjiu <77162816+maddietjiu@users.noreply.github.com>
2021-02-26 20:30:34 -0500

    Correction des caractères spéciaux

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -21,53 +21,51 @@ public class Client {
 		Scanner kb = new Scanner(System.in);
 		String serverAddress = sc.next();
 		while (true) {
-			// Tant que l'adresse ip n'est pas valide demander de entrer une autre adresse.
+			// Tant que l'adresse IP n'est pas valide demander d'entrer une autre adresse.
 			while (!ipValide(serverAddress)) {
 				System.out.println("Adresse IP invalide. Veuillez entrer un autre adresse IP valide:");
 				serverAddress = sc.next();
 			}
-			// Lire le port
+			// Lire le port entré
 			port = readPort(sc);
 
 			if (socket != null)
 				socket.close();
 
-			// 1. Crï¿½ation d'une nouvelle connexion avec le serveur
-			// String serverAddress="127.0.0.1";
-			// int port=5000;
-
+			// 1. Création d'une nouvelle connexion avec le serveur
+			
 			try {
 				socket = new Socket(serverAddress, port);
-				// Attendre que le client soit connectï¿½ au serveur
-				// Si la connection est timed out redemander au client d'entrer une nouvelle
+				// Attendre que le client soit connecté au serveur
+				// Si la connection est 'timed out', demander au client d'entrer une nouvelle
 				// adresse du serveur
 				synchronized (socket) {
 					socket.notify();
 				}
 			} catch (java.net.ConnectException e) {
-				System.out.println("Impossible de se connecter ï¿½ ce serveur");
+				System.out.println("Impossible de se connecter à ce serveur");
 				serverAddress = "";
 				port = 0;
 				continue;
 			}
 			break;
 		}
-		System.out.println("Le serveur opere sur l'adresse " + serverAddress + ":" + port);
+		System.out.println("Le serveur opère sur l'adresse " + serverAddress + ":" + port);
 
-		// Crï¿½ation d'un canal entrant pour recevoir les messages envoyï¿½s par le
+		// Création d'un canal entrant pour recevoir les messages envoyés par le
 		// serveur
 		DataInputStream in = new DataInputStream(socket.getInputStream());
 
-		// Attente de la rï¿½ception d'un message envoyï¿½ par le serveur sur le canal
+		// Attente de la réception d'un message envoyé par le serveur sur le canal
 		String messageFromServer = in.readUTF();
 		System.out.println(messageFromServer);
 
-		// Crï¿½ation d'un canal sortant pour ï¿½crire au serveur
+		// Création d'un canal sortant pour écrire au serveur
 		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 		PrintStream ps = new PrintStream(socket.getOutputStream());
 		String strKb = "";
 
-		// Saisir entrï¿½e du client au clavier
+		// Saisir entrée du client au clavier
 
 		while ((strKb = kb.nextLine()) != null) {
 
@@ -77,24 +75,24 @@ public class Client {
 			String[] command = strKb.split(" ");
 			switch (command[0]) {
 			case "upload":
-				// Si le client veut Upload un fichier au serveur
+				// Si le client veut télécharger un fichier au serveur
 				if (dir.contains(command[1])) { // N'envoyer le fichier que s'il existe
 					out.writeUTF("ready"); // Informe le serveur que le fichier demandé existe
 					dir.sendFile(command[1], out);
 					System.out.println("Le fichier " + command[1] + " a bien été téléversé.");
 				} else {
 					out.writeUTF("sorry");
-					System.out.println("le fichier " + command[1] + " n'existe pas");
+					System.out.println("Le fichier " + command[1] + " n'existe pas");
 				}
 				break;
 			case "download":
 
 				if (in.readUTF().equals("ready")) {
 					dir.saveFile(in, command[1]);
-					System.out.println("Le fichier " + command[1] + " a bien ï¿½tï¿½ tï¿½lï¿½chargï¿½.");
+					System.out.println("Le fichier " + command[1] + " a bien été téléchargé.");
 				} else
 					System.out.println(
-							"Aucun fichier nommï¿½ " + command[1] + " n'existe dans le dossier actuel du serveur");
+							"Aucun fichier nommé " + command[1] + " n'existe dans le dossier actuel du serveur.");
 				in.readNBytes(in.available());// Vider le InputStream
 				break;
 			default:
@@ -104,19 +102,20 @@ public class Client {
 			}
 			strKb = "";
 			command[0] = "";
-			if (command.length > 1) // Si la commande a un deuxieme argument on le rï¿½initialise
+			if (command.length > 1) // Si la commande a un deuxième argument, on le réinitialise
 				command[1] = "";
 		}
 
 		// Fermeture de la connexion avec le serveur
 		socket.close();
-		// close connections with read/write
+		// Fermeture des connexions pour lire et écrire
 		ps.close();
 		kb.close();
 		sc.close();
 	}
-
-	public static boolean ipValide(String serverAddress) {
+	
+	// Fonction qui vérifie le format de l'adresse IP entrée par l'utilisateur
+	public static boolean ipValide(String serverAddress) { 
 		if (serverAddress.endsWith("."))
 			return false;
 
@@ -154,11 +153,11 @@ public class Client {
 		;
 	}
 
-	// Inspirï¿½ de
+	// Inspiré de
 	// https://stackoverflow.com/questions/17985575/checking-if-input-from-scanner-is-int-with-while-loop-java
 	public static int readPort(Scanner sc) {
 		int port = 0;
-		System.out.println("Entrez numï¿½ro de port du serveur :");
+		System.out.println("Entrez numéro de port du serveur :");
 		if (sc.hasNextInt()) {
 			port = sc.nextInt();
 		} else {
@@ -166,11 +165,11 @@ public class Client {
 			port = 0;
 		}
 		while (port > 5050 || port < 5000) {
-			System.out.print("Numï¿½ro de port invalide. Veuillez enter un numï¿½ro de port entre 5000 et 5050 :");
+			System.out.print("Numéro de port invalide. Veuillez enter un numéro de port entre 5000 et 5050 :");
 			if (sc.hasNextInt()) {
 				port = sc.nextInt();
 			} else {
-				String buffer = sc.next();
+				sc.next(); // jeter l'entrée
 				port = 0;
 			}
 		}
